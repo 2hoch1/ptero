f5b8532bf55d511c8b1de78c960d0e6acaff02d7
2hoch1 <mail@2hoch1.dev>
2026-06-05 19:24:49 +0200

    refactor: restructure into monorepo

--- src/commands/init/lib/base.ts
+++
@@ -1,8 +0,0 @@
-import { run } from '@/commands/init/lib/common.js';
-
-export function installBaseDeps(): void {
-  run('apt-get update -y');
-  run(
-    'apt-get install -y curl wget gnupg lsb-release ca-certificates tar unzip git sudo openssl cron jq'
-  );
-}
--- src/commands/init/lib/composer.ts
+++
@@ -1,7 +0,0 @@
-import { run } from '@/commands/init/lib/common.js';
-
-export function installComposer(): void {
-  run(
-    'curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer'
-  );
-}
